bc5daa9c1f39a4d90f7eb640c9bc8a8ad7fa9ad8
ckcr4lyf <poiasdpoiasd@live.com>
2025-06-26 00:14:05 +0800

    Add logger, some logs

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,6 +1,11 @@
 import fs from 'node:fs';
+import { Logger, LOGLEVEL } from '@ckcr4lyf/logger'
 
 import { QobuzDlAPI } from "./api.mjs"
+
+export const getLogger = () => {
+    return new Logger({ loglevel: LOGLEVEL.INFO });
+}
 
 /**
  * 
@@ -8,7 +13,15 @@ import { QobuzDlAPI } from "./api.mjs"
  * @param {QobuzDlAPI} api 
  */
 export const downloadAlbum = async (albumId, api) => {
+    const logger = getLogger();
+
+    logger.info(`Going to get album info from Qobuz-DL instance (Album ID: ${albumId})`);
     const albumInfo = await api.getAlbumInfo(albumId);
+
+    logger.info(`Going to download album art from Qobuz`);
     const albumArt = await api.downloadAlbumArt(albumInfo);
-    fs.writeFileSync(`${albumId}_cover.jpg`, Buffer.from(albumArt));
+
+    const albumArtFilename = `${albumId}_cover.jpg`
+    fs.writeFileSync(albumArtFilename, Buffer.from(albumArt));
+    logger.info(`Wrote album art to ${albumArtFilename}`);
 }
