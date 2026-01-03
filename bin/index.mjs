d4277869c0c37ccbdfc1b3809fc16e3b52b8a931
Raghu Saxena <poiasdpoiasd@live.com>
2026-01-03 13:15:36 +0800

    Fix domain for quid.wtf

--- bin/index.mjs
+++ bin/index.mjs
@@ -7,10 +7,10 @@ import { downloadAlbum, getLogger } from "../src/utils.mjs";
 const logger = getLogger();
 
 if (process.argv.length < 3){
-  logger.warn(`Usage: qdc <album-id> (--format=m4a)`);
+  logger.warn(`Usage: qdc <album-id> (--format=alac)`);
   process.exit(-1);
 }
 
 // TBD: We should probably let the base url be configurable?!
-const api = new QobuzDlAPI("https://us.qobuz.squid.wtf");
+const api = new QobuzDlAPI("https://qobuz.squid.wtf");
 await downloadAlbum(process.argv[2], getFormat(process.argv), api);
